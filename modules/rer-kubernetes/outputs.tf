4c26a1e9c2f35139be8488efa88b73af497d116f
Olaf Krasicki-Freund <olaf.freund@outlook.com>
2021-01-31 15:49:29 +0000

    update

--- modules/rer-kubernetes/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The Kubernetes Managed Cluster ID."
-  value       = azurerm_kubernetes_cluster.aks.id
-}
-
-output "host" {
-  description = "The Kubernetes cluster server host."
-  value       = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
-}
--- modules/rer-storage-account/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terraform plan -var='containers=[{name="test", access_type="private"},]'
-terraform apply -var='containers=[{name="test", access_type="private"},]' --auto-approve
